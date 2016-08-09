df54fc4c5b5f15f292a4878a019fa483b3f20586
shenxy <yalunwang123@163.com>
2016-08-09 23:12:30 +0800

    提交redis类库

---
+++ RedisFramework/RedisKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaLunWang.RedisFramework
+{
+    public class RedisKeys
+    {
+        public static string SYSTEMLOG = "SYSTEMLOG";
+        public static string CARS = "CARS";
+        public static string STOPCARS = "STOPCARS";
+    }
+}
